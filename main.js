1a8be23d26f57419409a8ed15b5b4c7f5a3dacda
khumoyunhusanov <rashidovalutfiya@gmail.com>
2024-03-02 13:46:26 +0500

    main js concole

---
+++ main.js
@@ -0,0 +1,113 @@
+
+
+
+//  1
+
+let e = +prompt("first nomer");
+let s = +prompt("two nomer");
+let c = +prompt("javob");
+
+let d = e + s;
+
+if (c === d) {
+  console.log("true");
+} else {
+  console.log("falce");
+}
+
+//  2
+
+let text = "HELLO"
+let g = text.charAt(0)
+ if (text.charAt===text.charAt(0)) {
+    console.log(true);
+ } else {
+    console.log(false);
+ }
+
+//  3
+
+ let a = prompt("raqam yozing")
+ let b = prompt("ikkinchi raqamni yozing")
+
+ alert(parseInt(a) + parseInt(b))
+
+
+//  4
+
+ let m = +prompt("sonlarni qushing")
+ let l = +prompt("sonlarni tanlang")
+
+ console.log(m+l);
+
+//  5
+
+ let txt = "Salom";
+
+ if (txt === txt.toUpperCase()) {
+    console.log("True");
+  } else {
+     console.log("False");
+  }
+ 
+
+//  6
+  let harf = "Hayir"
+
+ if (harf.charAt(0) === harf.charAt(0).toUpperCase()) {
+   console.log("big");
+ } else {
+   console.log("small");
+}
+
+
+7
+
+let k = 50;
+
+ if (k > 100) {
+   console.log("katta");
+ } else {
+   console.log("kichik");
+ }
+
+
+//  8
+
+let y = +prompt("any number");
+ let j = +prompt("any number");
+
+ console.log(y - j);
+
+
+ 9
+
+let x = +prompt("first raqam");
+let v = +prompt("two raqam");
+let o = +prompt("javob");
+
+let p = x + v;
+
+if (o === p) {
+  console.log("to'g'ri");
+} else {
+  console.log("notug'ri");
+}
+
+
+// 10
+
+let harf1 = "Hello js";
+
+ if (harf1 === harf1.toUpperCase()) {
+     console.log("True");
+ } else {
+    console.log("False");
+
+ }
+
+
+
+
+
+
